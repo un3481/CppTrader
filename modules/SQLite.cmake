5a92a3c88a530ef7114e8c6ff1830e78ca774269
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 12:27:58 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,14 +1,8 @@
 
-# set (LIB_DIR_LOCATION ${NAME}/lib) 
-# find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION}) 
-# set(INCLUDE_DIRS ${NAME}/include)  
-#      
-# target_link_libraries(cpptrader SQLite3) 
-# target_include_directories(cpptrader PRIVATE ${INCLUDE_DIRS}) 
+set (LIB_DIR_LOCATION modules/SQLite)
+set(INCLUDE_DIRS modules/SQLite)
 
-find_package (SQLite3)
+find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION})
 
-if (SQLITE3_FOUND)
-  include_directories(${SQLITE3_INCLUDE_DIRS})
-  target_link_libraries (cpptrader ${SQLITE3_LIBRARIES})
-endif (SQLITE3_FOUND)
+target_link_libraries(cpptrader SQLite3)
+target_include_directories(cpptrader PRIVATE ${INCLUDE_DIRS})
