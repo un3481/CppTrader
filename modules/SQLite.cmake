5947b6ef93ab2d235f490f2ff2cdc4b72d4de0d9
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 12:40:22 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,8 +1,3 @@
 
-set (LIB_DIR_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
 set(INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
-
-find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION})
-
-target_link_libraries(sqlite3 SQLite3)
-target_include_directories(sqlite3 PRIVATE ${INCLUDE_DIRS})
+include_directories(${INCLUDE_DIRS})
