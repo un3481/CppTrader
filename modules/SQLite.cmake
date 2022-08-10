a795f17106a16b45d69cd32fcc0285e0dbab70cc
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 12:23:12 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,27 +1,14 @@
-# - Find sqlite3
-# Find the native SQLITE3 headers and libraries.
-#
-# SQLITE3_INCLUDE_DIRS	- where to find sqlite3.h, etc.
-# SQLITE3_LIBRARIES	- List of libraries when using sqlite.
-# SQLITE3_FOUND	- True if sqlite found.
 
-# Look for the header file.
-FIND_PATH(SQLITE3_INCLUDE_DIR NAMES sqlite3.h)
+# set (LIB_DIR_LOCATION ${NAME}/lib) 
+# find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION}) 
+# set(INCLUDE_DIRS ${NAME}/include)  
+#      
+# target_link_libraries(cpptrader SQLite3) 
+# target_include_directories(cpptrader PRIVATE ${INCLUDE_DIRS}) 
 
-# Look for the library.
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite)
+find_package (SQLite3)
 
-# Handle the QUIETLY and REQUIRED arguments and set SQLITE3_FOUND to TRUE if all listed variables are TRUE.
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
-
-# Copy the results to the output variables.
-IF(SQLITE3_FOUND)
-	SET(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
-	SET(SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
-ELSE(SQLITE3_FOUND)
-	SET(SQLITE3_LIBRARIES)
-	SET(SQLITE3_INCLUDE_DIRS)
-ENDIF(SQLITE3_FOUND)
-
-MARK_AS_ADVANCED(SQLITE3_INCLUDE_DIRS SQLITE3_LIBRARIES)
+if (SQLITE3_FOUND)
+  include_directories(${SQLITE3_INCLUDE_DIRS})
+  target_link_libraries (cpptrader ${SQLITE3_LIBRARIES})
+endif (SQLITE3_FOUND)
