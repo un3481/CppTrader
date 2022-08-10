c2eabcbef5e441a9eb42573e307ad6add16b3237
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 11:33:14 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,12 +1,13 @@
-if(NOT TARGET sqlite)
 
-  # Module flag
-  set(SQLITE_MODULE Y)
+cmake_minimum_required (VERSION 3.20)
+project (Tutorial)
 
-  # Module subdirectory
-  add_subdirectory("SQLite")
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
 
-  # Module folder
-  set_target_properties(sqlite PROPERTIES FOLDER "modules/SQLite")
+add_executable(tutorial new.cpp)
+find_package (SQLite3)
 
-endif()
+if (SQLITE3_FOUND)
+  include_directories(${SQLITE3_INCLUDE_DIRS})
+  target_link_libraries (tutorial ${SQLITE3_LIBRARIES})
+endif (SQLITE3_FOUND)
