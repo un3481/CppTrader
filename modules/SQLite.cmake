51385413e6f9267b18a7414842416a6b5aedbc72
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 11:59:06 -0300

    fix sqlite import

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -3,5 +3,5 @@ find_package (SQLite3)
 
 if (SQLITE3_FOUND)
   include_directories(${SQLITE3_INCLUDE_DIRS})
-  target_link_libraries (tutorial ${SQLITE3_LIBRARIES})
+  target_link_libraries (cpptrader ${SQLITE3_LIBRARIES})
 endif (SQLITE3_FOUND)
