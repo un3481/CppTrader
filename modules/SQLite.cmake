5fe21b1efaf8669fca6c567a3dba5433276e120e
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 12:35:01 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,8 +1,26 @@
 
-set (LIB_DIR_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
-set(INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
+if(NOT TARGET sqlite)
 
-find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION})
+  # Module library
+  file(GLOB SOURCE_FILES "SQLite/sqlite3.c")
+  add_library(sqlite ${SOURCE_FILES})
+  if(MSVC)
+    # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+    set_target_properties(sqlite PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4244")
+  else()
+    set_target_properties(sqlite PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  endif()
+  target_include_directories(sqlite PUBLIC "SQLite")
 
-target_link_libraries(cpptrader SQLite3)
-target_include_directories(cpptrader PRIVATE ${INCLUDE_DIRS})
+  # Module folder
+  set_target_properties(sqlite PROPERTIES FOLDER "modules/SQLite")
+
+endif()
+
+# set (LIB_DIR_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
+# set(INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
+# 
+# find_library(SQLite3 NAMES sqlite3 PATHS ${LIB_DIR_LOCATION})
+# 
+# target_link_libraries(sqlite SQLite3)
+# target_include_directories(sqlite PRIVATE ${INCLUDE_DIRS})
