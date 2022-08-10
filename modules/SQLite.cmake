e3d2651e3c0d223b4cc9c52fdbcfe1e5c55a3a7f
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-10 12:43:49 -0300

    Update SQLite.cmake

--- modules/SQLite.cmake
+++ modules/SQLite.cmake
@@ -1,3 +1,11 @@
 
-set(INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/modules/SQLite")
-include_directories(${INCLUDE_DIRS})
+if(NOT TARGET sqlite3)
+
+  # Module library
+  file(GLOB SOURCE_FILES "SQLite/sqlite3.c")
+  add_library(sqlite3 ${SOURCE_FILES})
+
+  set(INCLUDE_DIRS "SQLite")
+  include_directories(${INCLUDE_DIRS})
+
+endif()
